9fb78a6aaaf0da3fb2d3adb0e9f0cc4b6fb289d4
KyzmkyzTempest <ayaraprojectsgen2@gmail.com>
2025-04-21 18:23:24 +0700

    New 🌟

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
+app.use((req, res) => {
+  res.status(404).json({ status: "404", message: "Not Found" });
+});
+
 app.listen(8353, () => {
   console.log('Sistem Berjalan Di Port: 8353')
 })
